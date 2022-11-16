aab92f1fb45661446130e864f0712412fdfcfbf1
LostDev <82785402+iamlostdev@users.noreply.github.com>
2022-11-16 13:33:34 -0500

    Create post-install.bat

---
+++ post-install.bat
@@ -0,0 +1,5 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+choco install firefox onlyoffice qbittorrent vscodium discord steam
+
+iwr -useb https://christitus.com/win | iex
