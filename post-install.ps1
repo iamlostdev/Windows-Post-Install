a5b66543b4f77c350a1a6c2b2a7454111399f94a
LostDev <82785402+iamlostdev@users.noreply.github.com>
2022-11-22 03:37:43 +0000

    Update post-install.ps1

--- post-install.ps1
+++ post-install.ps1
@@ -1,5 +1,5 @@
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'));
 
-choco install librewolf onlyoffice qbittorrent vscodium discord steam lightshot keepassxc;
+choco install librewolf brave onlyoffice qbittorrent vscodium discord steam lightshot keepassxc winrar;
 
 iwr -useb https://christitus.com/win | iex;
